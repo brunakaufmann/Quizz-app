a07fcf478053025272c99e1b71393be8404b7c5d
Bruna Kaufmann <brunakaufmann1@gmail.com>
2021-03-28 13:07:01 -0300

    Getters & else-if

--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPhrase {
+    String resultText;
+    if (resultScore <= 8) {
+      resultText = 'Você é incrível e inocente';
+    } else if (resultScore <= 12) {
+      resultText = 'Você é legal';
+    } else if (resultScore <= 16) {
+      resultText = 'Você é ... estranho?!';
+    } else {
+      resultText = 'Você é ruim';
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Você Conseguiu!'),
+      child: Text(
+        resultPhrase,
+        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
