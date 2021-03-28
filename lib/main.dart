14344da085f0520afc9e0a78aa7463aecc3fc37e
Bruna Kaufmann <brunakaufmann1@gmail.com>
2021-03-28 13:16:33 -0300

    Resetting the Quiz

--- lib/main.dart
+++ lib/main.dart
@@ -49,8 +49,15 @@ class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
   var _totalScore = 0;
 
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
   void _answerQuestion(int score) {
-    _totalScore = _totalScore + score;
+    _totalScore += score;
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -75,7 +82,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(_totalScore),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
